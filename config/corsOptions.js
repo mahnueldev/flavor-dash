de52f319b1a12a7f2a2cc95f330dd317973e9c59
mahnueldev <emmanuelgbd3@gmail.com>
2023-07-07 06:17:32 +0100

    CORS

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -8,6 +8,12 @@ const corsOptions = {
     'http://localhost:8080/api/auth',
     'http://localhost:8080/api/refresh',
     'http://localhost:8080/api/recipes',
+    'https://flavourdash.netlify.app/',
+    'https://app-flavourdash.netlify.app/',
+    'https://determined-blue-cloak.cyclic.app',
+    'https://determined-blue-cloak.cyclic.app/api/auth',
+    'https://determined-blue-cloak.cyclic.app/api/refresh',
+    'https://determined-blue-cloak.cyclic.app/api/recipes',
   ],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
