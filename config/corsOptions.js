756a8c08752377e55be1f0a4fc48357694cb1420
mahnueldev <emmanuelgbd3@gmail.com>
2023-07-21 09:41:03 +0100

    cors fix

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,5 @@
 const corsOptions = {
   origin: [
-    // 'http://127.0.0.1:5173',
-    // 'http://127.0.0.1:8080',
-    // 'http://localhost:3000',
-    // 'http://localhost:3000',
-    // 'http://localhost:4000',
-    // 'http://localhost:8080',
-    // 'http://localhost:8080/api/auth',
-    // 'http://localhost:8080/api/refresh',
-    // 'http://localhost:8080/api/recipes',
-    
     'https://flavourdash.netlify.app',
     'https://app-flavourdash.netlify.app',
     'https://determined-blue-cloak.cyclic.app',
@@ -18,7 +8,7 @@ const corsOptions = {
     'https://determined-blue-cloak.cyclic.app/api/refresh',
     'https://determined-blue-cloak.cyclic.app/api/recipe',
   ],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token', 'x-api-key', 'x-api-host'],
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
