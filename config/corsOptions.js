28e845b0f650de75206f78a0f29b02dd225b427d
mahnueldev <emmanuelgbd3@gmail.com>
2023-08-28 22:32:18 +0100

    cors env

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,13 @@
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: '.env.development.local' });
+} else {
+  dotenv.config({ path: '.env.production.local' });
+}
+
 const corsOptions = {
-  origin: [
-    'https://cyclic.app',
-    'https://app.cyclic.sh/#',
-    'https://flavourdash.netlify.app',
-    'https://app-flavourdash.netlify.app',
-    'https://determined-blue-cloak.cyclic.app',
-    'https://determined-blue-cloak.cyclic.app/api/auth',
-    'https://determined-blue-cloak.cyclic.app/api/user',
-    'https://determined-blue-cloak.cyclic.app/api/refresh',
-    'https://determined-blue-cloak.cyclic.app/api/recipe',
-  ],
+  origin: process.env.ALLOWED_ORIGINS.split(','),
   optionsSuccessStatus: 200,
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token', 'x-api-key', 'x-api-host'],
   methods: 'GET,POST,PUT,DELETE',
